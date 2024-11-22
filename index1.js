f1c2d9ca1e80347aab87228f8a7d2f4104a2abce
Meri Mkhitaryan <meri.mkhitaryan03@gmail.com>
2024-11-23 02:35:28 +0400

    Add homework file:index1.js

--- index1.js
+++
@@ -1,9 +0,0 @@
-let firstString=prompt('Enter something');
-let outString='';
-for(i=0;i<firstString.length;i++) {
-if (firstString[i]===" "){
-                        outString +='-';
-}else{outString+=firstString[i];
-    
-}
-}
